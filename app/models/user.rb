e8c5c347a80c1021fcee4ce8f6e51b082d737c93
Chedak <cedomir.miskov@gmail.com>
2019-03-20 15:33:31 +0100

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, 
+                      presence: true,
+                      length: { maximum: 50 },
+                      uniqueness: {case_sensitive: false}
 end
